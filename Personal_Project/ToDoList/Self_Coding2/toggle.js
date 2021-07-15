976ccb8c650bef12da0789d189adb32a0faf0f6a
khsung <khsung0@nate.com>
2021-07-15 18:20:31 +0900

    Self_Coding2
    CSS 작성 중

--- Personal_Project/ToDoList/Self_Coding2/toggle.js
+++ Personal_Project/ToDoList/Self_Coding2/toggle.js
@@ -2,9 +2,9 @@ const clock_btn=clock_div.querySelector("button")
 
 clock_btn.addEventListener('click',function(){
     main_content.classList.toggle('active')
-    if (clock_btn.textContent==="닫기"){
-        clock_btn.innerHTML="열기"
+    if (clock_btn.textContent==="To Do List 닫기"){
+        clock_btn.innerHTML="To Do List 열기"
     }else{
-        clock_btn.innerHTML="닫기"
+        clock_btn.innerHTML="To Do List 닫기"
     }
 })
